ab616c6600adb15585ee5f2db776be0afc9d91a8
unknown <sobiab1267@gmail.com>
2026-08-29 19:18:06 +0500

    fix(dom): add DOM polyfill to prevent NotFoundError insertBefore crash from Google Translate

---
+++ client/src/utils/domPolyfill.ts
@@ -0,0 +1,31 @@
+/**
+ * Polyfill for Node.prototype.insertBefore and Node.prototype.removeChild
+ * 
+ * Prevents React DOM reconciliation crashes caused by Google Translate or browser
+ * extensions modifying/wrapping raw text nodes in <font> tags.
+ */
+if (typeof window !== 'undefined' && typeof Node !== 'undefined' && Node.prototype) {
+  const originalInsertBefore = Node.prototype.insertBefore;
+  Node.prototype.insertBefore = function <T extends Node>(newNode: T, referenceNode: Node | null): T {
+    if (referenceNode && referenceNode.parentNode !== this) {
+      if (referenceNode.parentNode) {
+        return referenceNode.parentNode.insertBefore(newNode, referenceNode);
+      }
+      return originalInsertBefore.call(this, newNode, null) as T;
+    }
+    return originalInsertBefore.call(this, newNode, referenceNode) as T;
+  };
+
+  const originalRemoveChild = Node.prototype.removeChild;
+  Node.prototype.removeChild = function <T extends Node>(child: T): T {
+    if (child.parentNode !== this) {
+      if (child.parentNode) {
+        return child.parentNode.removeChild(child) as T;
+      }
+      return child;
+    }
+    return originalRemoveChild.call(this, child) as T;
+  };
+}
+
+export {};
